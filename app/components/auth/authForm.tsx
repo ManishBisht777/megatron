b596e98aa41161dbe91ad070fce4c5089d2a13a7
manish bisht <manish205770@keshav.du.ac.in>
2023-04-16 21:02:42 +0530

    feat: github auth

--- app/components/auth/authForm.tsx
+++ app/components/auth/authForm.tsx
@@ -1,15 +1,60 @@
-import { cn } from "@/app/lib/utils";
+"use client";
+
 import React from "react";
+import { cn } from "@/app/lib/utils";
 import { buttonVariants } from "../ui/button";
 import { Icons } from "../ui/icons";
 import { Input } from "../ui/input";
 
+import { useSearchParams } from "next/navigation";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { signIn } from "next-auth/react";
+import { userAuthSchema } from "@/app/lib/validation/auth";
+
 type Props = {};
 
+type FormData = z.infer<typeof userAuthSchema>;
+
 const AuthForm = (props: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(userAuthSchema),
+  });
+
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [isGitHubLoading, setIsGitHubLoading] = React.useState<boolean>(false);
+  const searchParams = useSearchParams();
+
+  async function onSubmit(data: FormData) {
+    setIsLoading(true);
+
+    const signInResult = await signIn("email", {
+      email: data.email.toLowerCase(),
+      redirect: false,
+      callbackUrl: searchParams?.get("from") || "/",
+    });
+
+    if (!signInResult?.ok) {
+      console.log("something went wrong");
+    }
+
+    console.log("logged IN");
+
+    setIsLoading(false);
+  }
+
   return (
     <div className="flex flex-col gap-6">
-      <form action="" className="flex flex-col gap-2">
+      <form
+        action=""
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-2"
+      >
         <Input
           id="email"
           placeholder="name@example.com"
@@ -17,13 +62,11 @@ const AuthForm = (props: Props) => {
           autoCapitalize="none"
           autoComplete="email"
           autoCorrect="off"
-          //   disabled={isLoading || isGitHubLoading}
-          //   {...register("email")}
+          disabled={isLoading || isGitHubLoading}
+          {...register("email")}
         />
         <button className={cn(buttonVariants())}>
-          {/* {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )} */}
+          {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
           Sign In with Email
         </button>
       </form>
@@ -38,17 +81,17 @@ const AuthForm = (props: Props) => {
       <button
         type="button"
         className={cn(buttonVariants({ variant: "outline" }))}
-        // onClick={() => {
-        // setIsGitHubLoading(true)
-        // signIn("github")
-        // }}
-        // disabled={isLoading || isGitHubLoading}
+        onClick={() => {
+          setIsGitHubLoading(true);
+          signIn("github");
+        }}
+        disabled={isLoading || isGitHubLoading}
       >
-        {/* {isGitHubLoading ? (
+        {isGitHubLoading ? (
           <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-        ) : ( */}
-        <Icons.gitHub className="mr-2 h-4 w-4" />
-        {/* )}{" "} */}
+        ) : (
+          <Icons.gitHub className="mr-2 h-4 w-4" />
+        )}{" "}
         Github
       </button>
     </div>
---
+++ app/lib/auth.ts
@@ -0,0 +1,79 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "./db";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+};
+
+// Define a Session type
+type Session = {
+  user: User;
+};
+
+// Define a Token type
+type Token = {
+  id: string;
+  name: string;
+  email: string;
+  picture: string;
+};
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/auth/login",
+  },
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async session({
+      token,
+      session,
+    }: {
+      token: Token;
+      session: Session;
+    }): Promise<Session> {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await prisma.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id;
+        }
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+  },
+};
