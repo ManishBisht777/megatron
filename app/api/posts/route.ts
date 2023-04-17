7780432c970a1b657977d585aa7dac7e23fba2dc
manish bisht <manish205770@keshav.du.ac.in>
2023-04-17 19:28:45 +0530

    feat: add post

---
+++ app/api/posts/route.ts
@@ -0,0 +1,52 @@
+import { authOptions } from "@/app/lib/auth";
+import { prisma } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+import { getCurrentUser } from "../../lib/session";
+
+const postCreateSchema = z.object({
+  title: z.string(),
+  content: z.string().optional(),
+});
+
+export async function GET(request: Request) {
+  return NextResponse.json("hello");
+}
+
+export async function POST(req: Request, res: Response) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  try {
+    const body = await req.json();
+    const body2 = postCreateSchema.parse(body);
+    const post = await prisma.post.create({
+      data: {
+        title: body2.title,
+        content: body2.content,
+        authorId: currentUser.id,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    console.log(post);
+
+    return NextResponse.json(post);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.error();
+    }
+
+    // if (error instanceof RequiresProPlanError) {
+    //   return res.status(402).end()
+    // }
+
+    return NextResponse.error();
+  }
+}
--- app/components/dashboard/post-create-button.tsx
+++ app/components/dashboard/post-create-button.tsx
@@ -4,12 +4,27 @@ import React from "react";
 import { buttonVariants } from "@/app/components/ui/button";
 import { cn } from "@/app/lib/utils";
 import { Icons } from "@/app/components/ui/icons";
+import axios from "axios";
 
 interface PostCreateButtonProps
   extends React.HtmlHTMLAttributes<HTMLButtonElement> {}
 
 const PostCreateButton = ({ className, ...props }: PostCreateButtonProps) => {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  async function createPost() {
+    setIsLoading(true);
+
+    const response = await axios.post(
+      "/api/posts",
+      JSON.stringify({
+        title: "Untilted Post",
+      })
+    );
+    setIsLoading(false);
+
+    console.log(response.data);
+  }
 
   return (
     <button
@@ -22,6 +37,7 @@ const PostCreateButton = ({ className, ...props }: PostCreateButtonProps) => {
       )}
       disabled={isLoading}
       {...props}
+      onClick={createPost}
     >
       {isLoading ? (
         <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
